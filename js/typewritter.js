88aebe273f4e7457cbd3ee5a787549f12b8c3f08
Juan Rodriguez <juan.rodriguez@trackland.com>
2024-08-08 16:13:35 -0500

    f

--- js/typewritter.js
+++ js/typewritter.js
@@ -1,22 +1,17 @@
 let i = 0;
-let txt = '\n<Hola>\n Es todo \n  un placer!\n</Hola> \n';
+let txt = "\n<Hello>\n Nice to \n  meet you!\n</Hello> \n";
 
 let speed = 80;
-let executions = 0
+let executions = 0;
 
-let container = document.getElementById("heroCode1")
-
+let container = document.getElementById("heroCode1");
 
 function typeWriter() {
-
-    if (i < txt.length) {
-      document.getElementById("heroCode1").innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(typeWriter, speed);
-    }
-    
+  if (i < txt.length) {
+    document.getElementById("heroCode1").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
 }
-console.log(executions)
-typeWriter()
-
-
+console.log(executions);
+typeWriter();
